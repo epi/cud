eb40663b7cb132a366695a1f9a091653f7c257b2
Adrian Matoga <adrian@matoga.info>
2017-03-25 19:13:38 +0100

    preprocessor: implement #include and argumentless macros

--- source/cud/preprocessor.d
+++ source/cud/preprocessor.d
@@ -105,6 +105,31 @@ struct Token
 	string spelling; ///
 }
 
+private void popSpaces(R)(ref R tr) pure nothrow
+	if (isInputRange!R && is(ElementType!R : const(Token)))
+{
+	while (!tr.empty && tr.front.kind == TokenKind.space)
+		tr.popFront;
+}
+
+private Token expect(R)(ref R tr, TokenKind[] token_kinds...) pure
+{
+	Token result;
+	foreach (tk; token_kinds) {
+		if (tk != TokenKind.space)
+			tr.popSpaces;
+		if (!((!tr.empty && tr.front.kind == tk) || (tr.empty && tk == TokenKind.eof))) {
+			import std.conv : to;
+			throw new Exception("expected: " ~ to!string(tk));
+		}
+		if (result.kind == TokenKind.reserved)
+			result = tr.front;
+		if (!tr.empty)
+			tr.popFront;
+	}
+	return result;
+}
+
 private bool isSpace(char x) pure nothrow @nogc
 {
 	return x == ' ' || x == '\t' || x == '\0';
@@ -759,12 +784,12 @@ unittest
 }
 
 /**
-Preprocess a range of pp-tokens: execute preprocessor directives,
+Preprocess a file: execute preprocessor directives,
 convert pp-tokens into tokens.
 
 Params:
- input = input range of pp-`Token`s.
  fs = source file system, see `cud.preprocessor.isFS`
+ file_name = name of the file to preprocess
 
 Returns:
  Input range of `Token`s.
@@ -779,18 +804,188 @@ concatenation (6.10.3.3), the behavior is undefined. A `#include` preprocessing
 directive causes the named header or source file to be processed from phase 1
 through phase 4, recursively. All preprocessing directives are then deleted.)
 */
-auto preprocess(R, FS)(R input, auto ref FS fs)
-	if (isInputRange!R && is(ElementType!R : const(Token)) && isFS!FS)
+auto preprocess(FS)(FS fs, string file_name)
+	if (isFS!FS)
 {
+	import std.stdio;
 	static struct Preprocessor
 	{
-		private {
-			R input;
+		alias Lexer = typeof(lexFile(""));
+
+		static struct Macro
+		{
+			string name;
+			const(Token)[] arguments;
+			const(Token)[] tokens;
 		}
 
-		this(R input)
+		private {
+			FS fs;
+			Lexer[] lexerStack;
+			const(Token)[] line;
+			Macro[string] macros;
+		}
+
+		this(FS fs, string file_name)
 		{
-			this.input = input;
+			this.fs = fs;
+			stackPush(lexFile(file_name));
+			nextLine();
+		}
+
+		@property bool empty() const pure nothrow
+		{
+			return line.length == 0 && stackEmpty;
+		}
+
+		@property Token front() const pure nothrow
+		{
+			return line[0];
+		}
+
+		void popFront()
+		{
+			if ((line = line[1 .. $]).length == 0)
+				nextLine();
+		}
+
+		private auto lexFile(string name)
+		{
+			return split(fs[name], name).merge.tokenize;
+		}
+
+		private @property bool stackEmpty() pure nothrow const
+		{
+			return lexerStack.length == 0;
+		}
+
+		private void stackPop()
+		{
+			lexerStack = lexerStack[0 .. $ - 1];
+		}
+
+		private @property Lexer* stackTop() pure nothrow
+		{
+			return &lexerStack[$ - 1];
+		}
+
+		private void stackPush(Lexer i)
+		{
+			import std.algorithm : move;
+			lexerStack ~= move(i);
+		}
+
+		private void defineMacro(const(Token)[] tl)
+		{
+			auto name = tl.expect(TokenKind.identifier).spelling;
+			tl.popSpaces;
+			if (tl.empty) {
+				macros[name] = Macro(name);
+				return;
+			}
+			if (tl.front.kind == TokenKind.lparen)
+				assert(0, "TODO");
+			// C99 requires space after macro name, but we'll silently ignore this rule
+			const(Token)[] tokens;
+			substituteMacros(tl, tokens);
+			macros[name] = Macro(name, [], tokens);
+		}
+
+		private void substituteMacro(ref const Macro m)
+		{
+			foreach (t; m.tokens)
+				line ~= t;
+		}
+
+		private void substituteMacros(const(Token)[] from, ref const(Token)[] to)
+		{
+			foreach (t; from) {
+				switch (t.kind) {
+				case TokenKind.space: break;
+				case TokenKind.identifier:
+					if (auto pm = t.spelling in macros) {
+						substituteMacro(*pm);
+						break;
+					}
+					goto default;
+				default:
+					to ~= t;
+				}
+			}
+		}
+
+		private void includeFile(const(Token)[] tl)
+		{
+			auto name = tl.expect(TokenKind.headername, TokenKind.eof).spelling;
+			assert(name.length >= 2);
+			name = name[1 .. $ - 1];
+			stackPush(lexFile(name));
+		}
+
+		private const(Token)[] executePreprocessorDirective(const(Token)[] tl)
+		{
+			tl.popSpaces;
+			if (tl.empty)
+				return tl;
+			auto d = tl[0].spelling;
+			tl = tl[1 .. $];
+			switch (d) {
+				case "define":
+					defineMacro(tl);
+					return [];
+				case "include":
+					includeFile(tl);
+					return [];
+				default:
+					throw new Exception("invalid preprocessor directive #" ~ d);
+			}
+			//return tl;
+		}
+
+		private void nextLine()
+		{
+			const(Token)[] tl;
+			for (;;) {
+				while (!stackEmpty && stackTop.empty)
+					stackPop();
+				if (stackEmpty)
+					return;
+				auto lexer = stackTop;
+				while (!lexer.empty && lexer.front.kind == TokenKind.space)
+					lexer.popFront;
+				while (!lexer.empty) {
+					auto token = lexer.front;
+					lexer.popFront;
+					if (token.kind != TokenKind.newline)
+						tl ~= token;
+					else
+						break;
+				}
+				if (tl.empty)
+					continue;
+				if (tl.front.kind == TokenKind.hash) {
+					tl.popFront;
+					tl = executePreprocessorDirective(tl);
+					if (tl.empty)
+						continue;
+				}
+				substituteMacros(tl, line);
+				if (line.length)
+					return;
+				tl.length = 0;
+			}
 		}
 	}
+
+	return Preprocessor(fs, file_name);
+}
+
+unittest
+{
+	import std.algorithm : map, equal;
+	enum fs = [
+		"foo.h" : " #include <bar.h>\n\n\n  \t#  \t define BAZ BAR\nFOO BAZ FOO",
+		"bar.h" : "#\t\t\tdefine BAR 42\n#define FOO\t \t\t\n"
+	];
+	static assert(fs.preprocess("foo.h").map!(t => t.spelling).equal(["42"]));
 }
