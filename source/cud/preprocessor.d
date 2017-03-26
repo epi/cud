5204adda359fe3289e2244fe98b09536dc94138d
Adrian Matoga <adrian@matoga.info>
2017-03-26 14:01:46 +0200

    define function macros

--- source/cud/preprocessor.d
+++ source/cud/preprocessor.d
@@ -69,8 +69,9 @@ auto preprocess(FS)(FS fs, string file_name)
 		static struct Macro
 		{
 			string name;
-			const(Token)[] arguments;
-			const(Token)[] tokens;
+			const(Token)[] params;
+			const(Token)[] replacement;
+			bool isFunctionMacro;
 		}
 
 		private {
@@ -131,24 +132,27 @@ auto preprocess(FS)(FS fs, string file_name)
 
 		private void defineMacro(const(Token)[] tl)
 		{
-			auto name = tl.expect(TokenKind.identifier).spelling;
-			tl.popSpaces;
-			if (tl.empty) {
-				macros[name] = Macro(name);
-				return;
+			auto m = Macro(tl.expect(TokenKind.identifier).spelling);
+			if (!tl.empty && tl.front.kind == TokenKind.lparen) {
+				m.isFunctionMacro = true;
+				tl.popFront;
+				if (!tl.match(TokenKind.rparen)) {
+					for (;;) {
+						if (Token tok = tl.match(TokenKind.identifier)) {
+							m.params ~= tok;
+							if (tl.match(TokenKind.rparen))
+								break;
+							tl.expect(TokenKind.comma);
+						} else {
+							m.params ~= tl.expect(TokenKind.ellipsis, TokenKind.rparen);
+							break;
+						}
+					}
+				}
 			}
-			if (tl.front.kind == TokenKind.lparen)
-				assert(0, "TODO");
-			// C99 requires space after macro name, but we'll silently ignore this rule
-			const(Token)[] tokens;
-			substituteMacros(tl, tokens);
-			macros[name] = Macro(name, [], tokens);
-		}
-
-		private void substituteMacro(ref const Macro m)
-		{
-			foreach (t; m.tokens)
-				line ~= t;
+			// 6.10.3.3 requires space after macro name, but we'll silently ignore this rule
+			substituteMacros(tl, m.replacement);
+			macros[m.name] = m;
 		}
 
 		private void substituteMacros(const(Token)[] from, ref const(Token)[] to)
@@ -159,7 +163,8 @@ auto preprocess(FS)(FS fs, string file_name)
 					break;
 				case TokenKind.identifier:
 					if (auto pm = t.spelling in macros) {
-						substituteMacro(*pm);
+						foreach (mt; pm.replacement)
+							to ~= mt;
 						break;
 					}
 					goto default;
@@ -237,6 +242,64 @@ auto preprocess(FS)(FS fs, string file_name)
 unittest
 {
 	import std.algorithm : map, equal;
+	import cud.test;
+	crtest!("All forms of macro definitions are accepted",
+		() {
+			auto fs = [
+				"foo.h" :
+					"#define FOO \n" ~
+					"#define BAR 42 + bar\n" ~
+					"#define BAQ(  )\n" ~
+					"#define BAT(x)\n" ~
+					"#define BAZ(x, y)\n" ~
+					"#define BAL(...)\n" ~
+					"#define BAU(x, y, ...) (x - y) / baz(__VA_ARGS__)\n" ~
+					"#define BAN ( x ## y )" ];
+
+			auto pp = fs.preprocess("foo.h");
+			while (!pp.empty)
+				pp.popFront;
+			assert(!pp.macros["FOO"].isFunctionMacro);
+			assert(pp.macros["FOO"].params.length == 0);
+			assert(pp.macros["FOO"].replacement.length == 0);
+
+			assert(!pp.macros["BAR"].isFunctionMacro);
+			assert(pp.macros["BAR"].params.length == 0);
+			assert(pp.macros["BAR"].replacement.map!(t => t.spelling).equal(["42", "+", "bar"]));
+
+			assert(pp.macros["BAQ"].isFunctionMacro);
+			assert(pp.macros["BAQ"].params.length == 0);
+			assert(pp.macros["BAQ"].replacement.length == 0);
+
+			assert(pp.macros["BAT"].isFunctionMacro);
+			assert(pp.macros["BAT"].params.map!(t => t.spelling).equal(["x"]));
+			assert(pp.macros["BAT"].replacement.length == 0);
+
+			assert(pp.macros["BAZ"].isFunctionMacro);
+			assert(pp.macros["BAZ"].params.map!(t => t.spelling).equal(["x", "y"]));
+			assert(pp.macros["BAZ"].replacement.length == 0);
+
+			assert(pp.macros["BAL"].isFunctionMacro);
+			assert(pp.macros["BAL"].params.map!(t => t.spelling).equal(["..."]));
+			assert(pp.macros["BAL"].replacement.length == 0);
+
+			assert(pp.macros["BAU"].isFunctionMacro);
+			assert(pp.macros["BAU"].params.map!(t => t.spelling).equal(["x", "y", "..."]));
+			assert(pp.macros["BAU"].replacement.map!(t => t.spelling).equal([
+						"(", "x", "-", "y", ")", "/", "baz", "(", "__VA_ARGS__", ")"
+					]));
+
+			assert(!pp.macros["BAN"].isFunctionMacro);
+			assert(pp.macros["BAN"].params.length == 0);
+			assert(pp.macros["BAN"].replacement.map!(t => t.spelling).equal(["(", "x", "##", "y", ")"]));
+			return true;
+		});
+}
+
+unittest
+{
+	import std.algorithm : map, equal;
+
 	enum fs = [
 		"foo.h" : " #include <bar.h>\n\n\n  \t#  \t define BAZ BAR\nFOO BAZ FOO",
 		"bar.h" : "#\t\t\tdefine BAR 42\n#define FOO\t \t\t\n"
